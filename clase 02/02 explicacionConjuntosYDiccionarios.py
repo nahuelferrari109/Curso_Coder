075fb762a7922f8358d1cbfd3c846a8e05bb41bf
nahuelferrari109 <nahuelferrari109@gmail.com>
2025-06-12 13:42:36 -0300

    segunda clase

---
+++ clase 02/02 explicacionConjuntosYDiccionarios.py
@@ -0,0 +1,34 @@
+#CONJUNTOS Y DICCIONARIOS.
+#que son?
+#los conjuntos y diccionarios son otro tipo de colecciones que, al igual que las tuplas y listas, permiten almacenar multiples elementos en una sola variable.
+# Aunque ambos se utilizan para agrupar datos, tienen caracteristicas y usos distintos que los diferencian de otro tipos de colecciones como las listas y las tuplas.
+
+#diferencias y similuted
+# estructura
+#conjuntos: son colecciones no ordenadas de elemetnos unicos. No permiten duplicados y no mantienen el orden de los elemetnos.
+#Diccionarios: son colecciones de pares clave-valor. cada vlase es unica y se utiliza para almacenar y acceder a los valores correspondientes.
+
+#mutabilidad
+#Conjuntos: Son mutables, es decir, se pueden agregar o eliminar elemetnos despues de su creacio.
+#Diccionarios: Tambien son mutables, permitiendo la modificacion de par clave-valor existentes, asi como la adicion y eliminacion de nuevos pares.
+
+#acceso a elemetnos
+#conjuntos:No permiten el acceso a elementos mediante indice ya que no mantiene un orden.
+#Diccionario: Permiten el acceso rapido a los valores mediante sus claves unicas.
+
+#usos comunes.
+#conjuntos: se utilizan para alamacenar elementos unicos y realizar operaciones matematicas como uncion, interseccion y diferencia.
+#Diccionarios: Se utiliza para almacenar datos en pares clave-valor, permitendo busqueda rapidas y efecientes.
+
+#que son los conjuntos ?
+#Los conjuntos son colleciones no ordenadas de elementos unicos.
+# Esto segnifica que no pueden contener elementos duplicados y que el orden de los elemetnos no esta garantizados.
+
+#Sixtansis Basica 
+#para definir un conjuntos, se utilizan llaves {} o la funcion set(). Ejemplo.
+
+numeros = {1, 2, 3, 4, 5} #Definiendo un conjuntos de numeros.
+
+letras = set(["a", "b", "c", "d"]) #Usando la funcion set().
+
+conjuntos_vacio = set(); #Definimos un conjunto vacio.
